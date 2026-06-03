89224cc8e772e5629f2292c19bd4e96f4fee3112
14377 <nabiha.t101@gmail.com>
2026-06-03 16:58:51 -0400

    more imports

--- main.py
+++ main.py
@@ -5,5 +5,10 @@ import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
 
+fastf1.Cache.enable_cache('cache')
+
+session_2025 = fastf1.get_session(2025, 'Monaco', 'R')
+
+session_2025.load()
 
 
