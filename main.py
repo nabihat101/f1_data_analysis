4dcf3058e34bcc525d0c8aa396048e38fd5fbf1e
14377 <nabiha.t101@gmail.com>
2026-06-05 09:11:08 -0400

    loading fastf1 session data for the race and quali

--- main.py
+++ main.py
@@ -7,8 +7,18 @@ from sklearn.model_selection import train_test_split
 
 fastf1.Cache.enable_cache('cache')
 
-session_2025 = fastf1.get_session(2025, 'Monaco', 'R')
+rows = []
 
-session_2025.load()
+for year in [2022, 2023, 2024, 2025]:
+    race = fastf1.get_session(year, 'Monaco', 'R')
+    quali = fastf1.get_session(year, 'Monaco', 'Q')
+    race.load()
+    quali.load()
+
+    race_results = race.results
+    quali_results = quali.results
+
+
+
 
 
