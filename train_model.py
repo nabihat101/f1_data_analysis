f86266731bf3ffd2fecb2a56d91d619a875ea576
14377 <nabiha.t101@gmail.com>
2026-06-05 12:37:24 -0400

    restructuring files by splitting into training and predictor + adding functions to maintain organization

--- train_model.py
+++ train_model.py
@@ -3,6 +3,7 @@
 import fastf1
 import pandas as pd
 import os
+import numpy as np
 from sklearn.ensemble import GradientBoostingRegressor
 from sklearn.metrics import mean_absolute_error
 
@@ -25,11 +26,49 @@ def get_pace(session, driver):
 
     return laps['LapTime'].dt.total_seconds().median()
 
-rows = []
+def get_driver_features(fp1, fp2, quali, race, driver, weather, year):
+    """
+    Returns driver features
+    """
 
+    # instead of having indices of (0, 1, ...), set the indices to be driver names
+    race_results = race.results.set_index('Abbreviation')
+    quali_results = quali.results.set_index('Abbreviation')
+
+    # edge case
+    if driver not in race_results.index or driver not in quali_results.index:
+        return None
+
+    # get the specific information of each driver
+    r_driver = race_results.loc[driver]
+    q_driver = quali_results.loc[driver]
+
+    fp1_pace = get_pace(fp1, driver)
+    fp2_pace = get_pace(fp2, driver)
+
+    # return the data
+    return {
+        "year": year,
+        "driver": driver,
+
+        "fp1_pace": fp1_pace,
+        "fp2_pace": fp2_pace,
+        "pace_dif": fp1_pace - fp2_pace if pd.notnull(fp1_pace) and pd.notnull(fp2_pace) else np.nan,
+
+        "quali_pos": q_driver['Position'],
+        "grid_pos": r_driver['GridPosition'],
+
+        "team": r_driver['TeamName'],
+
+        "air_temp": weather["AirTemp"].mean(),
+        "track_temp": weather["TrackTemp"].mean(),
+        "rainfall": weather["Rainfall"].max(),
+        "finish_pos": r_driver['Position']
+        }
+
+rows = []
 for year in [2018, 2019, 2021, 2022, 2023, 2024, 2025]:
 
-    # get the sessions and load them for each year
     race = fastf1.get_session(year, 'Monaco', 'R')
     quali = fastf1.get_session(year, 'Monaco', 'Q')
     fp1 = fastf1.get_session(year, 'Monaco', 'FP1')
@@ -39,55 +78,24 @@ for year in [2018, 2019, 2021, 2022, 2023, 2024, 2025]:
     fp2.load()
     race.load()
     quali.load()
+
     weather = race.weather_data
 
-    # instead of having indices of (0, 1, ...), set the indices to be driver names
-    race_results = race.results.set_index('Abbreviation')
-    quali_results = quali.results.set_index('Abbreviation')
+    drivers = race.results['Abbreviation'].unique()
 
-    # loop through all drivers
-    for driver in race_results.index:
+    for driver in drivers:
+        feats = get_driver_features(fp1, fp2, quali, race, driver, weather, year)
 
-        if driver not in quali_results.index:
-            continue
-
-        # get the specific information of each driver
-        r_driver = race_results.loc[driver]
-        q_driver = quali_results.loc[driver]
-
-        fp1_pace = get_pace(fp1, driver)
-        fp2_pace = get_pace(fp2, driver)
-
-        # create a row for our new table
-        row = {
-            "year": year,
-            "driver": driver,
-            "fp1_pace": fp1_pace,
-            "fp2_pace": fp2_pace,
-            "pace_dif": fp1_pace - fp2_pace if pd.notnull(fp1_pace) and pd.notnull(fp2_pace) else np.nan,
-            "quali_pos": q_driver['Position'],
-            "grid_pos": r_driver['GridPosition'],
-            "team": r_driver['TeamName'],
-            "air_temp": weather["AirTemp"].mean(),
-            "track_temp": weather["TrackTemp"].mean(),
-            "rainfall": weather["Rainfall"].max(),
-            "finish_pos": r_driver['Position']
-        }
-
-        rows.append(row)
+        if feats is not None:
+            rows.append(feats)
 
 # cleaning data
+
 df = pd.DataFrame(rows)
 
 df = df.dropna()
 
 df = pd.get_dummies(df, columns=['driver', 'team'])
-
-# Create X (features )
-x = df.drop("finish_pos", axis=1)
-
-# Create y (labels)
-y = df["finish_pos"]
 
 # split the train and test based on years
 train_df = df[df["year"] <= 2023]
