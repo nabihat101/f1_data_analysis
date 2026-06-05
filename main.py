cf41482a7151d57fa7659a65912ad66abad055d4
14377 <nabiha.t101@gmail.com>
2026-06-05 10:34:03 -0400

    adding in training, testing, and fitting

--- main.py
+++ main.py
@@ -3,13 +3,17 @@
 import fastf1
 import pandas as pd
 import numpy as np
+import os
 from sklearn.model_selection import train_test_split
+from sklearn.ensemble import GradientBoostingRegressor
+from sklearn.metrics import mean_absolute_error
 
-fastf1.Cache.enable_cache('cache')
+os.makedirs("f1_cache", exist_ok=True)
+fastf1.Cache.enable_cache("f1_cache")
 
 rows = []
 
-for year in [2022, 2023, 2024, 2025]:
+for year in [2018, 2019, 2021, 2022, 2023, 2024, 2025]:
 
     # get the sessions and load them for each year
     race = fastf1.get_session(year, 'Monaco', 'R')
@@ -24,6 +28,9 @@ for year in [2022, 2023, 2024, 2025]:
     # loop through all drivers
     for driver in race_results.index:
 
+        if driver not in quali_results.index:
+            continue
+
         # get the specific information of each driver
         r_driver = race_results.loc[driver]
         q_driver = quali_results.loc[driver]
@@ -35,11 +42,37 @@ for year in [2022, 2023, 2024, 2025]:
 
             "quali_pos": q_driver['Position'],
             "grid_pos": r_driver['GridPosition'],
-            "team": r_driver['Team'],
+            "team": r_driver['TeamName'],
             "finish_pos": r_driver['Position']
         }
 
         rows.append(row)
+
+df = pd.DataFrame(rows)
+
+df = pd.get_dummies(df, columns=['driver', 'team'])
+
+# Create X (features )
+x = df.drop("finish_pos", axis=1)
+
+# Create y (labels)
+y = df["finish_pos"]
+
+# split the train and test based on years
+X_train = x[df["year"] <= 2023]
+X_test = x[df["year"] > 2023]
+
+y_train = y[df["year"] <= 2023]
+y_test = y[df["year"] > 2023]
+
+# using this model so that it learns and improves score
+clf = GradientBoostingRegressor(n_estimators=300, learning_rate=0.05, max_depth=3, random_state=42)
+
+# fitting the model
+clf.fit(X_train, y_train)
+pred = clf.predict(X_test)
+
+print("MAE:", mean_absolute_error(y_test, pred))
 
 
 
