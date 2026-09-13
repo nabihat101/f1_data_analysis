309f7ed0bf5b5bd7874f1c3ce33d5442bdc6fae9
Nabiha Tariq <nabiha.t101@gmail.com>
2026-09-13 08:51:38 -0400

    changing to global model

--- features_utils.py
+++ features_utils.py
@@ -1,42 +1,56 @@
-
-def get_driver_features(fp1, fp2, weather, driver, year, quali=None, race=None, training=False):
+def get_driver_features(
+        fp1,
+        fp2,
+        weather,
+        driver,
+        year,
+        track,
+        quali=None,
+        race=None,
+        training=False
+):
     """
-    Gets all the necessary driver features
+    Creates the features used to predict a driver's race result.
     """
 
-    results = fp1.results.set_index("Abbreviation")
+    fp1_results = fp1.results.set_index("Abbreviation")
+    fp2_results = fp2.results.set_index("Abbreviation")
+    quali_results = quali.results.set_index("Abbreviation")
 
-    if driver not in results.index:
+    # Error handling: Make sure the driver has data from all required sessions
+    if driver not in fp1_results.index:
         return None
 
-    d = results.loc[driver]
-    fp2_results = fp2.results.set_index("Abbreviation")
+    if driver not in fp2_results.index:
+        return None
 
-    fp1_place = results.loc[driver]["Position"]
-    fp2_place = fp2_results.loc[driver]["Position"]
+    if driver not in quali_results.index:
+        return None
 
-    quali_results = quali.results.set_index("Abbreviation")
+    driver_info = fp1_results.loc[driver]
 
     row = {
         "year": year,
+        "track": track,
         "driver": driver,
 
-        "fp1_pace": fp1_place,
-        "fp2_pace": fp2_place,
-
+        "fp1_pos": fp1_results.loc[driver]["Position"],
+        "fp2_pos": fp2_results.loc[driver]["Position"],
         "quali_pos": quali_results.loc[driver]["Position"],
 
-        "team": d["TeamName"],
+        "team": driver_info["TeamName"],
 
         "air_temp": weather["AirTemp"].mean(),
         "track_temp": weather["TrackTemp"].mean(),
         "rainfall": weather["Rainfall"].max(),
     }
 
-    # if training, use more available information
-
+    # During training, we know the actual race result.
     if training:
         race_results = race.results.set_index("Abbreviation")
+
+        if driver not in race_results.index:
+            return None
 
         row["finish_pos"] = race_results.loc[driver]["Position"]
 
