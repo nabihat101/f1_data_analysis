cd67eeb7972c04a92817a9e69e176e52f81846c6
Nabiha Tariq <nabiha.t101@gmail.com>
2026-09-20 15:09:12 -0400

    add comments to js

--- frontend/script.js
+++ frontend/script.js
@@ -1,23 +1,39 @@
+
+/**
+ * predicts race by connecting the frontend to the backend and fetching the prediction data + waiting for it
+*/
+
 async function predictRace() {
+
+    // find the track input and results container in the DOM
     const track = document.getElementById("track").value.trim();
     const results = document.getElementById("results");
 
+    // ensures user enters race
     if (!track) {
         results.innerHTML = '<p class="error">Please enter a race.</p>';
         return;
     }
 
+    // while waiting for the prediction, show a loading message
     results.innerHTML = "<p>Loading prediction...</p>";
 
+    // error handling for the fetch request to the backend
     try {
+
+        // waiting for python to send the prediction data back to the frontend
         const response = await fetch(`http://127.0.0.1:8000/predict?track=${encodeURIComponent(track)}`);
+        
+        // waiting for the response to be converted to JSON format
         const data = await response.json();
 
+        // if the backend returns an error, display it to the user
         if (data.error) {
             results.innerHTML = `<p class="error">${data.error}</p>`;
             return;
         }
 
+        // creates row to be displayed in frontend
         let html = `
             <h2>Predicted ${data.track} ${data.year} Results</h2>
             <table>
@@ -31,6 +47,7 @@ async function predictRace() {
                 <tbody>
         `;
 
+        // the columns to be displayed 
         data.predictions.forEach(prediction => {
             html += `
                 <tr>
@@ -41,9 +58,15 @@ async function predictRace() {
             `;
         });
 
+        // add to table
         html += "</tbody></table>";
+
+        // add to html
         results.innerHTML = html;
-    } catch (error) {
+    } 
+    
+    // error handling for if the fetch request fails
+    catch (error) {
         results.innerHTML = '<p class="error">Could not connect to the prediction server.</p>';
     }
 }
